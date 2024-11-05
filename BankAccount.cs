d57b75cb19b171edecd7f316682c1b167f64b691
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-11-05 16:26:36 +0000

    Add implementation for BankAccount
    BankAccount now has functionality for depositing and withdrawing money, with respect to an overdraft limit. The code
    is also made cleaner with private set accessors for the properties, so that they can only be set within the class.
    
    Also add boilerplate code for BankingTransaction and BankingSystem classes
    
    Begin banking system implementation
    
    Begin banking system implementation

---
+++ BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NcBankingSystem
+{
+    internal class BankAccount
+    {
+        public int AccountNumber { get; private set; }
+        public int Balance { get; private set; }
+        public string Name { get; private set; }
+
+        public int ArrangedOverdraft { get; private set; }
+
+        public BankAccount(int accountNumber, string name, int arrangedOverdraft)
+        {
+            AccountNumber = accountNumber;
+            Name = name;
+            Balance = 0;
+            ArrangedOverdraft = arrangedOverdraft;
+        }
+
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(int amount)
+        {
+            int newBalance = Balance - amount;
+            if (newBalance >= ArrangedOverdraft * -1)
+            {
+                Balance = newBalance;
+            }
+        }
+    }
+}
