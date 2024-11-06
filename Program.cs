33047b2a546261a0a780a20ad901ff345fb979f4
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-06 14:43:32 +0000

    Update testing code in Program.cs

--- Program.cs
+++ Program.cs
@@ -10,12 +10,40 @@
             bankingSystem.CreateAccount("Kate", 1000);
             bankingSystem.CreateAccount("Ben", 1000);
             bankingSystem.CreateAccount("Jack", 1000);
-            bankingSystem.PerformDeposit(0, 1000);
-            bankingSystem.PerformDeposit(0, 100);
-            bankingSystem.PerformWithdrawal(0, 1000);
-            bankingSystem.PerformDeposit(0, 250);
+            Console.WriteLine("Accounts created:");
             bankingSystem.ListAccounts();
-            bankingSystem.ListTransactions(0);
+
+            Console.WriteLine();
+            Console.WriteLine("Now performing transactions on Ben's account");
+            bankingSystem.PerformDeposit(2, 500);
+            bankingSystem.ListTransactions(2);
+            Console.WriteLine();
+            bankingSystem.PerformWithdrawal(2, 1500);
+            bankingSystem.ListTransactions(2);
+            Console.WriteLine();
+            bankingSystem.PerformWithdrawal(2, 900);
+            bankingSystem.ListTransactions(2);
+            Console.WriteLine();
+            bankingSystem.PerformWithdrawal(2, 100);
+            bankingSystem.ListTransactions(2);
+
+            Console.WriteLine();
+            Console.WriteLine("Current accounts status:");
+            bankingSystem.ListAccounts();
+
+            Console.WriteLine();
+            Console.WriteLine("Now performing transactions on Ben's account");
+            bankingSystem.PerformDeposit(2, 1);
+            bankingSystem.ListTransactions(2);
+            Console.WriteLine();
+            bankingSystem.PerformWithdrawal(2, 2);
+            bankingSystem.ListTransactions(2);
+
+            Console.WriteLine();
+            Console.WriteLine("Current accounts status:");
+            bankingSystem.ListAccounts();
+
+
         }
     }
 }
