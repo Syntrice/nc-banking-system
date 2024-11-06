41d8957958d1b6d47207d15b2b44d39d959d8d59
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-06 13:58:09 +0000

    Remove redundant using statements

--- BankingSystem.cs
+++ BankingSystem.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Transactions;
-
-namespace NcBankingSystem
+﻿namespace NcBankingSystem
 {
     internal class BankingSystem
     {
